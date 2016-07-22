82a348544b6ef3130c46f803e476c85ebf587385
Erica Chia <echia3@gatech.edu>
2016-07-21 20:56:11 -0400

    Added a logo, search bar, and a button. UI testing purposes only, no functionality. -Erica

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,47 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Thomas the Search Engine");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text sceneTitle = new Text("Thomas the Search Engine");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        TextField userTextField = new TextField();
+        userTextField.setPromptText("Search");
+        GridPane.setConstraints(userTextField, 1,1);
+        grid.getChildren().add(userTextField);
+
+        Button submit = new Button("Submit");
+        GridPane.setConstraints(submit, 2,1);
+        grid.getChildren().add(submit);
+
+        Scene scene = new Scene(grid, 300,275);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
